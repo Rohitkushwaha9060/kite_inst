8da910e7eb918fa60c0b736ff764d9812bbe1e7b
Rohit Kushwaha <rajdilkhush0987654321@gmail.com>
2024-07-06 15:56:00 +0530

    remove turbo repo

--- apps/center-dashboard/tailwind.config.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const sharedConfig = require("@repo/tailwind-config/tailwind.config.js");
-
-module.exports = {
-  ...sharedConfig,
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-};
--- apps/main/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-const sharedConfig = require("@repo/tailwind-config/tailwind.config.js");
-
-module.exports = {
-  ...sharedConfig,
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-};
